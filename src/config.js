beeb6516104d9236f44e6e8b53069d7f21867077
Willy Mello <cwmello165@gmail.com>
2019-03-29 12:22:49 -0400

    heroku??

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
-const API_URL = 'http://localhost:8080'
+const API_URL = process.env.NODE_ENV === 'production'
+  ? 'https://pacific-fortress-71039.herokuapp.com/'
+  : 'http://localhost:8080'
 const CLIENT_URL = 'http://localhost:3000'
+
+//https://pacific-fortress-71039.herokuapp.com/ 
 
 module.exports = { API_URL, CLIENT_URL }
